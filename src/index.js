39984f36c2574ef6e3762dfbb1716315ab41d170
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-05-01 23:32:50 +0200

    refactor: render all dom elements from js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@ import {Buttons} from "./js/Buttons";
 
 
 window.onload = function () {
+    const container = document.createElement('div')
+    container.classList.add('container')
+    const h1 = document.createElement('h1')
+    h1.innerText = 'Virtual Keyboard';
+    const textarea = document.createElement('textarea');
+    const keyboard = document.createElement('div')
+    keyboard.classList.add('keyboard')
+    textarea.rows = '10';
+    container.append(h1)
+    container.append(textarea)
+    container.append(keyboard)
+    document.querySelector('body').append(container)
     renderButtonsToDom()
+
 
 }
 
 
 const renderButtonsToDom = () => {
+
     const keyboard = document.querySelector('.keyboard')
     generateButton(macKeyboardRU).forEach(btn => {
         keyboard.append(btn.generateButtons())
